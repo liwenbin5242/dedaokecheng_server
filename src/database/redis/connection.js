0b230109f099637fc74adc3769e76acc58366bc8
ubuntu <ubuntu@localhost.localdomain>
2021-02-05 14:56:18 +0800

    init

---
+++ src/database/redis/connection.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const nconf = require('nconf');
+const redis = require('redis');
+const winston = require('winston');
+const _ = require('lodash');
+
+const connection = module.exports;
+
+connection.getConnectionOptions = function (redis) {
+	redis = redis || nconf.get('redis');
+	const connOptions = {};
+	if (redis.password) {
+		connOptions.auth_pass = redis.password;
+	}
+	if (redis.hasOwnProperty('database')) {
+		connOptions.db = redis.database;
+	}
+	return _.merge(connOptions, redis.options || {});
+};
+
+connection.connect = async function (options) {
+	return new Promise(function (resolve, reject) {
+		options = options || nconf.get('redis');
+		const redis_socket_or_host = options.host;
+		const connOptions = connection.getConnectionOptions(options);
+
+		let cxn;
+		if (redis_socket_or_host && String(redis_socket_or_host).indexOf('/') >= 0) {
+			/* If redis.host contains a path name character, use the unix dom sock connection. ie, /tmp/redis.sock */
+			cxn = redis.createClient(options.host, connOptions);
+		} else {
+			/* Else, connect over tcp/ip */
+			cxn = redis.createClient(options.port, options.host, connOptions);
+		}
+
+		const dbIdx = parseInt(options.database, 10);
+		if (!(dbIdx >= 0)) {
+			throw new Error('[[error:no-database-selected]]');
+		}
+
+		cxn.on('error', function (err) {
+			winston.error(err.stack);
+			reject(err);
+		});
+		cxn.on('ready', function () {
+			resolve(cxn);
+		});
+
+		if (options.password) {
+			cxn.auth(options.password);
+		}
+	});
+};
+
+require('../../promisify')(connection);
---
+++ src/database/redis/pubsub.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const nconf = require('nconf');
+const util = require('util');
+const winston = require('winston');
+const EventEmitter = require('events').EventEmitter;
+const connection = require('./connection');
+
+let channelName;
+const PubSub = function () {
+	const self = this;
+	channelName = 'db:' + nconf.get('redis:database') + ':pubsub_channel';
+
+	connection.connect().then(function (client) {
+		self.subClient = client;
+		self.subClient.subscribe(channelName);
+		self.subClient.on('message', function (channel, message) {
+			if (channel !== channelName) {
+				return;
+			}
+
+			try {
+				var msg = JSON.parse(message);
+				self.emit(msg.event, msg.data);
+			} catch (err) {
+				winston.error(err.stack);
+			}
+		});
+	});
+
+	connection.connect().then(function (client) {
+		self.pubClient = client;
+	});
+};
+
+util.inherits(PubSub, EventEmitter);
+
+PubSub.prototype.publish = function (event, data) {
+	this.pubClient.publish(channelName, JSON.stringify({ event: event, data: data }));
+};
+
+module.exports = new PubSub();
